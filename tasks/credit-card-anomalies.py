d49b26991197c1e181fa3b1b57df80553a41d84a
ne5ermind <stepankutaev@yandex.ru>
2026-01-20 13:56:51 +0300

    neovim btw

--- tasks/credit-card-anomalies.py
+++ tasks/credit-card-anomalies.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.express as px
+from sklearn.ensemble import IsolationForest
 
 # %%
 
@@ -30,4 +32,36 @@ plt.boxplot(data_amt)
 plt.ylabel("значения")
 plt.title("boxplot")
 plt.show()
+# %%
+
+temp = data[["category", "amt"]]
+
+data_enc = pd.get_dummies(temp, columns=["category"])
+
+model = IsolationForest(contamination=0.05, random_state=67)
+
+data_enc["anomaly"] = model.fit_predict(data_enc)
+# %%
+
+temp = data_enc.columns[1:-1]
+
+dummy_cols = data_enc[list(temp)]
+
+data_enc["category"] = pd.from_dummies(dummy_cols)
+data_enc = data_enc.drop(columns=temp)
+
+anomalies_iso = data_enc[data_enc["anomaly"] == -1]
+anomalies_iso
+# %%
+
+fig = px.strip(
+    data_enc,
+    x="anomaly",
+    y="amt",
+    color="category",
+    hover_data=["category", "amt"],
+    title="анализ аномалий в банковских переводах по типам покупок",
+)
+fig.update_layout(yaxis_type="log")
+fig.show()
 # %%
